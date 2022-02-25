2c9510bb6331b8549e9e0e64276cab331c3a0045
Sabino Eduardo <bilson@outlook.com.br>
2022-02-24 22:43:57 -0300

    Update nota_Fiscal.sql

--- nota_Fiscal.sql
+++ nota_Fiscal.sql
@@ -71,3 +71,39 @@ select * from compras;   /* Selecionando todos os registros da tabela */
         from compras;
 
 /*e) Pesquise o valor total das NF e ordene o resultado do maior valor para o menor. As colunas presentes no resultado da consulta são: ID_NF, VALOR_TOTAL. OBS: O VALOR_TOTAL é obtido pela fórmula: ∑ QUANTIDADE * VALOR_UNIT. Agrupe o resultado da consulta por ID_NF.*/
+
+        select id_nf, sum(quantidade*valor_unit) as valor_total 
+        from compras
+        group by id_nf
+        order by valor_total desc;
+
+/*f) Pesquise o valor vendido das NF e ordene o resultado do maior valor para o menor. As
+colunas presentes no resultado da consulta são: ID_NF, VALOR_VENDIDO. OBS: O
+VALOR_TOTAL é obtido pela fórmula: ∑ QUANTIDADE * VALOR_UNIT. O
+VALOR_VENDIDO é igual a ∑ VALOR_UNIT - (VALOR_UNIT*(DESCONTO/100)). Agrupe
+o resultado da consulta por ID_NF.*/
+
+/*g) Consulte o produto que mais vendeu no geral. As colunas presentes no resultado da
+consulta são: COD_PROD, QUANTIDADE. Agrupe o resultado da consulta por
+COD_PROD.*/
+
+/*h) Consulte as NF que foram vendidas mais de 10 unidades de pelo menos um produto.
+As colunas presentes no resultado da consulta são: ID_NF, COD_PROD, QUANTIDADE.
+Agrupe o resultado da consulta por ID_NF, COD_PROD.*/
+
+/*i) Pesquise o valor total das NF, onde esse valor seja maior que 500, e ordene o
+resultado do maior valor para o menor. As colunas presentes no resultado da consulta
+são: ID_NF, VALOR_TOT. OBS: O VALOR_TOTAL é obtido pela fórmula: ∑ QUANTIDADE
+* VALOR_UNIT. Agrupe o resultado da consulta por ID_NF.*/
+
+/*j) Qual o valor médio dos descontos dados por produto. As colunas presentes no
+resultado da consulta são: COD_PROD, MEDIA. Agrupe o resultado da consulta por
+COD_PROD.*/
+
+/* k) Qual o menor, maior e o valor médio dos descontos dados por produto. As colunas
+presentes no resultado da consulta são: COD_PROD, MENOR, MAIOR, MEDIA. Agrupe
+o resultado da consulta por COD_PROD.
+l) Quais as NF que possuem mais de 3 itens vendidos. As colunas presentes no resultado
+da consulta são: ID_NF, QTD_ITENS. OBS:: NÃO ESTÁ RELACIONADO A QUANTIDADE
+VENDIDA DO ITEM E SIM A QUANTIDADE DE ITENS POR NOTA FISCAL. Agrupe o
+resultado da consulta por ID_NF. */
