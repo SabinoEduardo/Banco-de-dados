559a131cbb9b87412051bc1da9e7fd794a2eee36
Sabino Eduardo <bilson@outlook.com.br>
2022-02-24 22:37:09 -0300

    Update nota_Fiscal.sql

--- nota_Fiscal.sql
+++ nota_Fiscal.sql
@@ -70,4 +70,4 @@ select * from compras;   /* Selecionando todos os registros da tabela */
         convert(decimal(5,2),(valor_unit*quantidade - (valor_unit*quantidade*desconto/100))) as valor_vendido
         from compras;
 
-/* e) Pesquise o valor total das NF e ordene o resultado do maior valor para o menor. As colunas presentes no resultado da consulta são: ID_NF, VALOR_TOTAL. OBS: O VALOR_TOTAL é obtido pela fórmula: ∑ QUANTIDADE * VALOR_UNIT. Agrupe o resultado da consulta por ID_NF.*/
+/*e) Pesquise o valor total das NF e ordene o resultado do maior valor para o menor. As colunas presentes no resultado da consulta são: ID_NF, VALOR_TOTAL. OBS: O VALOR_TOTAL é obtido pela fórmula: ∑ QUANTIDADE * VALOR_UNIT. Agrupe o resultado da consulta por ID_NF.*/
