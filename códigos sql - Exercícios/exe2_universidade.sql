dc8ef5e539f23acc4ecb2e42349f612b8a87a8ec
Sabino Eduardo <bilson@outlook.com.br>
2022-02-27 17:16:41 -0300

    Update exe2_universidade.sql

--- códigos sql - Exercícios/exe2_universidade.sql
+++ códigos sql - Exercícios/exe2_universidade.sql
@@ -15,13 +15,51 @@ create database universidade     /* Criando banco de dados */
 
 use universidade; /* Usando o banco de dados */
 
-create table alunos (
-    matricula numeric(10) not NULL primary key,
-    nome varchar(40) not null,
-    endereco varchar(45),
-    cidade VARCHAR(20)
-);
+/* Criando a tabela alunos */
+
+    create table alunos (
+        matricula numeric(10) not NULL primary key,
+        nome varchar(45) not null,
+        endereco varchar(50),
+        cidade VARCHAR(20)
+    );
 
 
-exec sp_columns alunos;
+/* Criando a tabela disciplinas */
 
+    create table disciplinas (
+        cod_disciplina  varchar(3) not null primary key,
+        nome_disciplina varchar(50) not null,
+        carga_horária   numeric(2)
+    );
+
+/* Criando a tabela professores */
+
+    create table professores (
+        cod_professor numeric(10) not null primary key,
+        nome_professor varchar(45) not null,
+        endereco varchar(50),
+        cidade varchar(20)
+    );
+
+/* Criando a tabela turmas */
+
+    create table turma (
+        cod_disciplina  varchar(3) not null,
+        cod_turma numeric(10) not null,
+        cod_professor numeric(10) not null,
+        ano numeric(10) not null,
+        horário varchar(7),
+
+        constraint turma_cod_disciplina_fk foreign key(cod_disciplina) references disciplinas(cod_disciplina),
+
+        constraint turma_cod_professor_fk foreign key(cod_professor) references professores(cod_professor),
+
+        constraint turma_pk primary key 
+        (cod_disciplina, cod_turma, cod_professor, ano)
+    );
+
+
+    exec sp_columns turma;
+
+    
